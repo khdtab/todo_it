ddbf80bd117a4b37651b47af3ab5ecb98e12606c
khdta <khd.tayeb@gmail.com>
2020-12-28 11:28:42 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int age=15;
+        if (age > 18) {
+            System.out.println( "im an adult" );
+        }else{
+            System.out.println( "you are not adult" );
+        }
+
+
+    }
+}
